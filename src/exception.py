17668bab1ed42ab628d8c3f33df0f96188d91c5d
Ipsita Gayatri <gitipsita1234@gmail.com>
2025-06-10 22:05:53 +0530

    add logging and exception handler

---
+++ src/exception.py
@@ -0,0 +1,19 @@
+import sys
+import logging
+import logger
+
+def get_error_message(error,error_details:sys):
+    _,_,exc_tb=error_details.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message=f"Error occured in python script: {file_name}, line number: {exc_tb.tb_lineno}, error message: {str(error)}"
+    return error_message 
+
+#create custom exceptions
+class CustomException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        super().__init__(error_message)
+        self.error_message=get_error_message(error_message,error_details=error_details)
+
+    def __str__(self):
+        return self.error_message
+    
